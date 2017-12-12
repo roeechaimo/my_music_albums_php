9ec24447e1df32682e7d510886695697a547e958
roeechaimo <roee84@gmail.com>
2017-12-12 19:54:37 +0200

    set player controls and add option for playing next/previous

--- client/my-music-albums-php/src/app/components/player/player.component.ts
+++ client/my-music-albums-php/src/app/components/player/player.component.ts
@@ -15,11 +15,13 @@ export class PlayerComponentt implements OnInit {
   //TODO - handle with album image error on init
   //TODO - fix style of player
   album;
+  songsArray;
   albumToCheck;
   audio;
+  playingSong;
+  showPlay: boolean = false;
+  showPause: boolean = true;
   subscription: any;
-
-  //TODO - set player controls and add option for playing next/previous
 
   ngOnInit() {
     this.album = this._loadAlbumToPlayer.getLoadedAlbum();
@@ -35,18 +37,68 @@ export class PlayerComponentt implements OnInit {
       return;
     }
     this.album = album;
+    this.songsArray = this.album.playlistDetails;
+    this.playingSong = this.songsArray[0];
+    this.playAudio(this.album, 0);
+    this.showPlay = false;
+    this.showPause = true;
+    console.log(this.album);
+  }
+
+  playAudio(album, songIndex) {
     if (this.audio !== undefined) {
       this.audio.pause();
       this.audio = undefined;
     }
-    this.playAudio(this.album);
-    console.log(this.album);
+    this.audio = new Audio();
+    this.audio.src = album.playlistDetails[songIndex].song_src;
+    this.audio.play();
   }
 
-  playAudio(album) {
-    this.audio = new Audio();
-    this.audio.src = album.playlistDetails[0].song_src;
-    this.audio.play();
+  pauseSong() {
+    if (this.audio) {
+      this.audio.pause();
+      this.showPause = false;
+      this.showPlay = true;
+    }
+  }
+
+  keepPlayingSong() {
+    if (this.audio) {
+      this.audio.play();
+      this.showPlay = false;
+      this.showPause = true;
+    }
+  }
+
+  playNextSong() {
+    let songOrderObj = this.checkNextAndBackSongs(this.playingSong);
+    this.playAudio(this.album, this.songsArray.indexOf(songOrderObj.nextSongIndex));
+    this.playingSong = songOrderObj.nextSongIndex;
+    this.showPause = true;
+    this.showPlay = false;
+  }
+
+  playPreviousSong() {
+    let songOrderObj = this.checkNextAndBackSongs(this.playingSong);
+    this.playAudio(this.album, this.songsArray.indexOf(songOrderObj.previousSongIndex));
+    this.playingSong = songOrderObj.previousSongIndex;
+    this.showPause = true;
+    this.showPlay = false;
+  }
+
+  checkNextAndBackSongs(playingSong) {
+    if (this.album) {
+      let currentSongIndex = this.songsArray.indexOf(playingSong);
+      let previousSongIndex = this.songsArray[currentSongIndex == 0 ? this.songsArray.length - 1 : currentSongIndex - 1];
+      let nextSongIndex = this.songsArray[currentSongIndex == this.songsArray.length -1 ? 0 : currentSongIndex + 1];
+      let songOrderObj = {
+        "currentSongIndex": currentSongIndex,
+        "previousSongIndex": previousSongIndex,
+        "nextSongIndex": nextSongIndex
+      }
+      return songOrderObj;
+    }
   }
 
   ngOnDestroy() {
